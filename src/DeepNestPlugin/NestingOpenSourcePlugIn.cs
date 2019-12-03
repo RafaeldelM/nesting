d37c7dc4ca12814aeab55333bc92ac3ed66a87bb
Rafa <rafael@2shapes.com>
2019-12-03 01:21:09 +0100

    Adding Docking Panel for Nesting Selection

--- src/DeepNestPlugin/NestingOpenSourcePlugIn.cs
+++ src/DeepNestPlugin/NestingOpenSourcePlugIn.cs
@@ -1,4 +1,9 @@
-﻿namespace NestingOpenSource
+﻿using System;
+using Rhino.PlugIns;
+using RhinoWindows.Controls;
+using DockBarPanel = NestingOpenSource.Panel.DockBarPanel;
+
+namespace NestingOpenSource
 {
     ///<summary>
     /// <para>Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
@@ -9,8 +14,11 @@
     /// "Show All Files" to see it in the "Solution Explorer" window).</para>
     ///</summary>
     public class NestingOpenSourcePlugIn : Rhino.PlugIns.PlugIn
-
     {
+
+        private WpfDockBar m_wpf_bar;
+
+
         public NestingOpenSourcePlugIn()
         {
             Instance = this;
@@ -22,8 +30,45 @@
             get; private set;
         }
 
+        protected override LoadReturnCode OnLoad(ref string errorMessage)
+        {
+
+            CreateMyDockBar();
+
+            return base.OnLoad(ref errorMessage);
+        }
+
+        private void CreateMyDockBar()
+        {
+            var create_options = new DockBarCreateOptions
+            {
+                DockLocation = DockBarDockLocation.Right,
+                Visible = false,
+                DockStyle = DockBarDockStyle.Any,
+                FloatPoint = new System.Drawing.Point(100, 100)
+            };
+
+
+            m_wpf_bar = new WpfDockBar();
+            m_wpf_bar.Create(create_options);
+        }
+
+
         // You can override methods here to change the plug-in behavior on
         // loading and shut down, add options pages to the Rhino _Option command
         // and maintain plug-in wide options in a document.
+    }
+
+
+    /// <summary>
+    /// WpfDockBar dockbar class
+    /// </summary>
+    internal class WpfDockBar : DockBar
+    {
+        public static Guid BarId => new Guid("{5faf6cd0-c387-46cd-83b3-1445c126cf55}");
+        public WpfDockBar() : base(NestingOpenSourcePlugIn.Instance, BarId, "DeepNest")
+        {
+            SetContentControl(new WpfHost(new DockBarPanel(), null));
+        }
     }
 }
---
+++ src/DeepNestPlugin/Panel/SampleCsDockbarCommand.cs
@@ -0,0 +1,16 @@
+﻿using Rhino;
+using Rhino.Commands;
+
+namespace NestingOpenSource.Panel
+{
+  public class SampleCsDockbarCommand : Command
+  {
+    public override string EnglishName => "Deepnest";
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      RhinoWindows.Controls.DockBar.Show(WpfDockBar.BarId, false);
+      return Result.Success;
+    }
+  }
+}
